d11ef2a1288a02d6535f8f20d24486a533fcc8d6
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-09-21 18:24:22 +0600

    Problem11TwoPointer(O(n))

--- LeetCodeSolve/Medium/Problem11.cpp
+++ LeetCodeSolve/Medium/Problem11.cpp
@@ -3,34 +3,83 @@
 
 using namespace std;
 
-
-
-//Brute Force Approach
+//Two Pointer Approach
 int maxArea(vector <int> &height){
-    int i = 0;
     int n = height.size();
-    int maxarea = INT_MIN;
+    int i = 0;
+    int j = n-1;
+    int maxWater= 0;
 
-    for(i = 0; i < n; i++){
-        for(int j = 0; j < n ; j++){
-            int width = j - i;
-            int maxHeight = min(height[i], height[j]);
-            cout << "Width : " << width << endl;
-            cout << "Height : " << maxHeight << endl;
-            int currenTArea = maxHeight * width;
-            if(currenTArea > maxarea){
-                maxarea = currenTArea;
-            }
+    while(i < j){
+        cout << "i :" << i << endl;
+        cout << "j :" << j << endl;
+
+        int width = j - i;
+        int currentHeight = min(height[i], height[j]);
+        int currentArea = currentHeight * width;
+        if(height[i] < height[j]){
+            i++;
         }
+        else {
+            j--;
+        }
+        if (currentArea > maxWater){
+            maxWater = currentArea;
+        }
+        cout << "Width : " << width << endl; 
+        cout << "currentHeight : " << currentHeight << endl; 
+        cout << "currentArea : " << currentArea << endl; 
     }
-    return maxarea;
+    return maxWater;
+
+
+    // for(i = 0; i < n; i++){
+    //     int width = j - i;
+    //     int currentHeight = min(height[i], height[j]);
+    //     int currentArea = currentHeight * width;
+    //     if(height[i] < height[j]){
+    //         j++;
+    //     }
+    //     else{
+    //         i++;
+    //     }
+    //     if (currentArea > maxWater){
+    //         maxWater = currentArea;
+    //     }
+        
+    // }
+    // return maxWater;
 }
 
 
 
+
+
+//Brute Force Approach -> Time Complexity (worst case - > TLE)
+// int maxArea(vector <int> &height){
+//     int i = 0;
+//     int n = height.size();
+//     int maxarea = INT_MIN;
+
+//     for(i = 0; i < n; i++){
+//         for(int j = 0; j < n ; j++){
+//             int width = j - i;
+//             int maxHeight = min(height[i], height[j]);
+//             cout << "Width : " << width << endl;
+//             cout << "Height : " << maxHeight << endl;
+//             int currenTArea = maxHeight * width;
+//             if(currenTArea > maxarea){
+//                 maxarea = currenTArea;
+//             }
+//         }
+//     }
+//     return maxarea;
+// }
+
+
+
 int main(){
-    vector <int> height = {1,8,6,2,5,4,8,3,7};
-     
+    vector <int> height = {1,1};
     cout << maxArea(height);
 
 }
