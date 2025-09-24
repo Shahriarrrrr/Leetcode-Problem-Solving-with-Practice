31eb97eb27fd98850ce14fd1bf43b68ba9f2fca5
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-09-24 17:14:36 +0600

    RecursionBinarySearch

---
+++ Searching/BinarySearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector <int> &nums, int target, int start, int end){
+    if (start >= end) return -1;
+    int mid = (start+end) / 2;
+    if(target == nums[mid]){
+        return mid;
+    }
+    else if(target < nums[mid]){
+        return binarySearch(nums, target, start, mid);
+    }
+    else{
+        return binarySearch(nums, target, mid+1, end);
+    }
+    return -1;
+}
+
+
+
+int main(){
+vector <int> nums = {-1,0,3,4,5,9,12};
+    int end = nums.size();
+    int start = 0;
+    int target = 11;  
+    cout << binarySearch(nums, target, start,  end);
+}
