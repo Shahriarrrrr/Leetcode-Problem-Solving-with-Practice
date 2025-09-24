dc2df8f3e63b6ccf10118dd8f0eaf83b5c9f9c30
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-09-24 17:19:14 +0600

    RecursionBinarySearchOptimization

--- Searching/BinarySearch.cpp
+++ Searching/BinarySearch.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int binarySearch(vector <int> &nums, int target, int start, int end){
     if (start >= end) return -1;
-    int mid = (start+end) / 2;
+    int mid = start + (end - start) / 2;
     if(target == nums[mid]){
         return mid;
     }
     else if(target < nums[mid]){
-        return binarySearch(nums, target, start, mid);
+        return binarySearch(nums, target, start, mid-1);
     }
     else{
         return binarySearch(nums, target, mid+1, end);
@@ -20,9 +20,9 @@ int binarySearch(vector <int> &nums, int target, int start, int end){
 
 
 int main(){
-vector <int> nums = {-1,0,3,4,5,9,12};
+vector <int> nums = {-1,0,3,4,5,9,12,13};
     int end = nums.size();
     int start = 0;
-    int target = 11;  
+    int target = 13;  
     cout << binarySearch(nums, target, start,  end);
 }
