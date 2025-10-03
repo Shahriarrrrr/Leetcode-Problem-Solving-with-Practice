9216cc1d53c9cff6ba56078053226c5661002972
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-03 19:48:41 +0600

    Problem852

---
+++ LeetCodeSolve/Medium/Problem852.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+    int start = 0;
+    int end = arr.size() - 1 ;
+
+    while(start < end){
+        int mid = start + (end - start) / 2 ;
+        if(arr[mid+1] > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+        
+    }
+    return start;
+    
+
+}
+
+
+
+int main(){
+    vector <int> arr = {3,5,3,2,0};
+    cout << peakIndexInMountainArray(arr) << endl;
+}
