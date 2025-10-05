895eb1e903ada1b9ad785615acc68cedc5b6148d
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-05 23:42:42 +0600

    BookAllocationProblemDone

--- OutOfTheBoxProblems/BinarySearch/BookAllocation.cpp
+++ OutOfTheBoxProblems/BinarySearch/BookAllocation.cpp
@@ -33,3 +33,60 @@ Constraints:
 1 <= m <= n
 -----------------------------------------------
 */
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isValid(vector <int> arr, int  maxAllowedPages, int n, int m){
+    int st = 1;
+    int pages = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > maxAllowedPages){
+            return false;
+        } 
+        if(pages + arr[i] <= maxAllowedPages){
+            pages+=arr[i];
+        } else{
+            st++;
+            pages = arr[i];
+        }
+    }
+    return st > m ? false : true;
+}
+
+
+int allocatebooks(vector <int> &arr , int n , int m){
+    if(m > n){
+        return -1;
+    }
+    int sum = 0;
+    for(int item : arr){
+        sum+=item;
+    }
+    int ans = - 1;
+    int start = 0;
+    int end = sum;
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        if(isValid(arr, mid , n, m)){
+            ans = mid;
+            end = mid - 1;
+        } else{
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
+
+
+int main(){
+    vector <int> arr = {2,1,3,4};
+    int n  = 4 ;
+    int m = 2;
+
+    cout << allocatebooks(arr, n, m) << endl;
+}
