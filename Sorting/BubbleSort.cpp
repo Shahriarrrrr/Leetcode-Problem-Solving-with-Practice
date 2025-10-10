0e36b5aad7355f8c3fafab50f190075092254fac
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-10 19:05:50 +0600

    SelectionSort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 
 void bubbleSort(int arr[], int n){
+    bool isSwap = false;
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n - i - 1 ; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap){
+            return;
         }
     }
 }
