e6afd82576e8105486393b19e0354b0bcd07a0ca
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-11-02 21:23:26 +0600

    TweaksMadeto88

--- LeetCodeSolve/Easy/Problem88.cpp
+++ LeetCodeSolve/Easy/Problem88.cpp
@@ -3,29 +3,29 @@
 
 using namespace std;
 
-
 void merge(vector <int>& nums1, int m, vector <int>& nums2, int n){
-    int idx = m+n -1;
-    int j = n-1;
-    int i = m-1;
+        int idx = m+n -1;
+        int j = n-1;
+        int i = m-1;
 
-    while(i > 0 && j >= 0){
-        if(nums2[j] > nums1[i]){
-            nums1.at(idx) = nums2[j];
-            idx--;
-            j--;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                nums1.at(idx) = nums1[i];
+                i--;
+                idx--;
+            }
+            else{
+                nums1.at(idx) = nums2[j];
+                j--;
+                idx--;
+            }
         }
-        else if(nums1[i] >= nums2[j]){
-            nums1.at(idx) = nums1[i];
-            i--;
-            idx--;
+            while(j >= 0){
+                nums1[idx--] = nums2[j--];
+            }
 
-        }
+
     }
-
-
-}
-
 
 
 int main(){
